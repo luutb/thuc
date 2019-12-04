0e486cd9436baea351ea3c5747b687bf75efbedd
Luu oc cho <nguyebaluu0603@gmail.com>
2019-12-04 18:58:35 +0700

    thucdemo

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webApi.Models;
+
+namespace webApi.Controllers
+{
+    public class LoginController : ApiController
+    {
+        testEntities db = new testEntities();
+        [HttpPost]
+        public ApiResponse<Boolean> login([FromBody] login data)
+        {
+            if (db.logins.Where(m => m.username == data.username && m.password == data.password).Any())
+            {
+                return new ApiResponse<Boolean> { error = 0, message = "LOGIN SUCCESS", data = true };
+            }
+            return new ApiResponse<Boolean> { error = 1, message = "LOGIN FAILED", data = false };
+        }
+
+        [HttpPost]
+        public ApiResponse<Boolean> exist([FromBody] login data)
+        {
+            if (db.logins.Where(m => m.username == data.username).Any())
+            {
+                return new ApiResponse<Boolean> { error = 0, message = "LOGIN SUCCESS", data = true };
+            }
+            return new ApiResponse<Boolean> { error = 1, message = "LOGIN FAILED", data = false };
+        }
+
+
+    }
+}
---
+++ Models/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webApi.Models
+{
+    public class ApiResponse<T>
+    {
+        public int error;
+        public string message;
+        public T data;
+    }
+}
